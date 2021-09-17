1ab09ab67d335e3abf672aca4bd0cc1ab25af3c7
한송희 <98_han@naver.com>
2021-09-17 15:39:31 +0900

    Randomisation exercise

--- code exercise - Randomisation.playground/Contents.swift
+++ code exercise - Randomisation.playground/Contents.swift
@@ -10,7 +10,7 @@ func exercise() {
     
     
 
-    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     //The number of letters in alphabet equals 26
     
     let a = alphabet[Int.random(in: 0 ... 25)]
