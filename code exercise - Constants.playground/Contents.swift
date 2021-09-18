77bdede86a3c046c08327ed649a76d90d44c4176
한송희 <98_han@naver.com>
2021-09-18 22:14:02 +0900

    code exercise Constant

---
+++ code exercise - Constants.playground/Contents.swift
@@ -0,0 +1,17 @@
+// Variables, Constant
+
+// Declare a constant called secondsInAnHour tat represents the number of seconds in an hour and assign that number to it on the same line. The data type of the constant should be an Integer.
+
+
+
+func exercise() {
+    //Write your code here.
+    
+    let hour = 60
+    let secondsInAnHour = Int(hour*60)
+
+    
+    //Don't change the code below.
+    print(secondsInAnHour)
+
+}
