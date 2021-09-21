c10d7c300ebda3c63428d047583dbcc612530951
한송희 <98_han@naver.com>
2021-09-21 18:24:10 +0900

    make magic 8 ball use random function

--- Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet var imgView: UIImageView!
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func btnAskPressed(_ sender: UIButton) {
+        let randomNum = Int.random(in: 0 ... 4)
+        imgView.image = ballArray[randomNum]
+        
+        
     }
-
-
+ 
+    
 }
 
