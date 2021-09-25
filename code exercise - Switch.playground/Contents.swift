74b04a6bee39b17170ebb25db5d587ce9c4a1268
한송희 <98_han@naver.com>
2021-09-25 14:02:22 +0900

    code exercise - Switch

---
+++ code exercise - Switch.playground/Contents.swift
@@ -0,0 +1,28 @@
+var aNumber =  Int(readLine()!)!
+
+func dayOfTheWeek(day: Int) {
+  
+    switch day {
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    default:
+        print("Error")
+        
+    }
+}
+
+dayOfTheWeek(day:1)
+
+
