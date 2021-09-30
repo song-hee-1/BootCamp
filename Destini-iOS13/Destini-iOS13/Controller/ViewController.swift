13d351ca26b1bfaed70f0551cebd05d179303202
한송희 <98_han@naver.com>
2021-09-30 22:22:02 +0900

    9/30

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let array = Story(t: "You see a fork in the road", c1: "Take a left", c2: "Take a right")
+    
 
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
@@ -16,8 +19,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story[title]
+        choice1Button.setTitle.(story[c1], for: <#T##UIControl.State#>)
+        choice2Button.setTitle(choice2, for: .normal)
     }
+        
+    
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
+    
 
 
 }
